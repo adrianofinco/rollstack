24239c302552c3d4948fafd23c6d711168285099
adrianofinco <adriano.finco.509@gmail.com>
2020-12-06 20:49:58 -0300

    docs(types): add type definitions

---
+++ src/index.d.ts
@@ -0,0 +1,15 @@
+/**
+ * Generates a random float number that can be used for seeding
+ */
+export function generateSeed(): number
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * @param min Minimal value (inclusive)
+ * @param max Maximum value (inclusive)
+ */
+export function randomNumber(min: number, max: number): number
+/**
+ * Returns a random string with specified length
+ * @param length String length
+ */
+export function randomString(length: number): string
